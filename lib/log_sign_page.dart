663d41da19a8153551f695449156a9ecbe133acd
ahmdmlndy47 <ahmdmlndy47@gmail.com>
2026-05-11 12:55:03 +0300

    update log or sign page

--- lib/log_sign_page.dart
+++ lib/log_sign_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'components/main_button.dart';
 // صفحة تسجيل الدخول او إضافة ايميل و كلمة مرور
 class LogOrSignPage extends StatefulWidget {
@@ -15,51 +14,73 @@ class _LogOrSignPageState extends State<LogOrSignPage> {
     return  Scaffold(
         body: Container(
           // صورة الخلفبة للصفحة
+          padding: EdgeInsets.symmetric(vertical: 40),
           decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage('images/logOrSignPage.jpg'),
               fit: BoxFit.cover,
             ),
           ),
-          child: Center(
-            child: Container(
-              padding: EdgeInsets.all(30),
-              width: 300,
-              height: 300,
-              decoration: BoxDecoration(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
                   color: Colors.black54,
-                  borderRadius: BorderRadius.circular(20)
+                ),
+                child: Text(
+                  "مستوصف الخير",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
               ),
-              // الازرار
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  // زر تسجبل الدخول
-                  MyButton(
-                      onPressed: (){
-                          Navigator.of(context).pushNamed("loginPage");
+              SizedBox(height: 80,),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(30),
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  // الازرار
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      // زر تسجبل الدخول
+                      MyButton(
+                          onPressed: (){
+                            Navigator.of(context).pushNamed("loginPage");
                           },
-                      fontSize: 26,
-                      label: "تسجيل الدخول",
-                      shape: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50),
-                            borderSide: BorderSide(width: 0,color: Colors.transparent)
-                      )),
-                  //زر اضافة الايميل و كلمة المرور
-                  MyButton(
-                      onPressed: (){
-                          Navigator.of(context).pushNamed("signupPage");
-                        },
-                      fontSize: 26,
-                      label: "إنشاء حساب",
-                      shape: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          borderSide: BorderSide(width: 0,color: Colors.transparent)
-                      )
-                    ),
-                ],
+                          fontSize: 26,
+                          label: "تسجيل الدخول",
+                          shape: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(width: 0,color: Colors.transparent)
+                          )),
+                      //زر اضافة الايميل و كلمة المرور
+                      MyButton(
+                          onPressed: (){
+                            Navigator.of(context).pushNamed("signupPage");
+                          },
+                          fontSize: 26,
+                          label: "إنشاء حساب",
+                          shape: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(width: 0,color: Colors.transparent)
+                          )
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
         )
     );
