291a00bfba50e6488f8aa4417c0b2dcc72e20034
ahmdmlndy47 <ahmdmlndy47@gmail.com>
2026-05-15 20:42:28 +0300

    add the appointment page

--- lib/appointment_page.dart
+++ lib/appointment_page.dart
@@ -22,6 +22,113 @@ class _AppointmentPageState extends State<AppointmentPage> {
           ),
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color:  Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(-5, 5),
+                  blurRadius: 5
+                ),
+
+              ]
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "موعدك الحالي",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Text(
+                  "لديك موعد بالعيادة العينية",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Container(
+                  decoration: BoxDecoration(
+                    border: BoxBorder.all(
+                      color: Colors.red,
+                      width: 2
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.black
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 10,horizontal: 30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "دورك الحالي",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 18,
+                            color: Colors.red
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      Text(
+                        "21",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  textDirection: TextDirection.rtl,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "الوقت المتبقي تقريبا 2ساعة",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextButton(
+                          onPressed: (){},
+                          child: Text(
+                            "إلغاء الموعد",
+                            style:TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 12
+                            ),
+                          )),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 import 'log_sign_page.dart';
 void main() {
   runApp( MaterialApp(
-    home: AdminHomepage(),
+    home: Homepage(),
     debugShowCheckedModeBanner: false,
     routes: {
       "homepage" : (context) => Homepage(),
